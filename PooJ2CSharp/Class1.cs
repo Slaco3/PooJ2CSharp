4ac8dbce681c40100e2f38db07c8019ee97cd9f9
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2024-03-26 09:25:11 +0100

    Ajoutez des fichiers projet.

---
+++ PooJ2CSharp/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooJ2CSharp
+{
+    class Animal
+    {
+        public string Name;
+
+
+        public Animal()
+        {
+            this.Name = "Animal";
+        }
+        public Animal(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public void Moove()
+        {
+            Console.WriteLine("Moove");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Sleep");
+        }
+
+
+
+    }
+}
---
+++ PooJ2CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PooJ2CSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal animalRandom = new Animal();
+        Animal animalLion = new Animal("Simba");
+
+        animalRandom.Moove();
+        animalLion.Moove();
+    }
+}
